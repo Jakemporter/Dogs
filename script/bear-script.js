d5ae6b77d1e6960aab3d548280692b662daa469a
Jake Porter <Jake.m.porter@gmail.com>
2022-08-20 17:30:50 -0500

    almost done with bears page just need to add photos

---
+++ script/bear-script.js
@@ -0,0 +1,14 @@
+function displayAge() {
+  var dob = moment("15-05-2020", "DD-MM-YYYY");
+  var today = moment();
+  var monthDiff = today.diff(dob, "months");
+  var yearDiff = today.diff(dob, "years");
+  var i = 0;
+  var months = 0;
+  while (i < yearDiff) {
+    months += 12;
+    i++;
+  }
+  $("#age").append(` ${yearDiff} Years and ${monthDiff - months} months old`);
+}
+displayAge();
